bbd1780432deea42614cb46c12670fccc582113e
Lokesh3177 <lokesh31770@gmail.com>
2026-06-18 14:20:10 +0530

    Fix User model

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,16 +7,14 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
       trim: true,
-      match:[/\S+@\S+\.\S+/, "Please use a valid email address"],
+      match: [/\S+@\S+\.\S+/, "Please use a valid email address"],
     },
-
     password: {
       type: String,
       required: true,
@@ -28,4 +26,5 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports =
+  mongoose.models.User || mongoose.model("User", userSchema);
